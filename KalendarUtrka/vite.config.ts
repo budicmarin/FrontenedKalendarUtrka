151a14d2a5f8e603a9d47185fd4891262a02071e
budicmarin <budicmarin1@gmail.com>
2026-04-18 21:46:17 +0200

    novo

--- KalendarUtrka/vite.config.ts
+++ KalendarUtrka/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FrontenedKalendarUtrka/',
+  base: '/KalendarUtrka/',
   plugins: [
     vue(),
     vueDevTools(),
